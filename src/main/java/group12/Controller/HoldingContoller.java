1987b226bca83cfd17d7a483a54db370b0a373e2
Jennifer <ruiz.jenni11@icloud.com>
2026-09-21 21:26:44 +0000

    scrum-37: implement Holdings table API endpoints

---
+++ src/main/java/group12/Controller/HoldingContoller.java
@@ -0,0 +1,45 @@
+package group12.Controller;
+
+import org.springframework.web.bind.annotation.*;
+import group12.Entities.HoldingEntity;
+import group12.Services.HoldingService;
+import org.springframework.http.ResponseEntity;
+import lombok.RequiredArgsConstructor;
+import java.util.List;
+
+
+@RestController 
+@RequestMapping("api/holdings")
+@RequiredArgsConstructor 
+public class HoldingContoller {
+
+    private final HoldingService holdingService;
+
+    @GetMapping("/{holdingId}")
+    public ResponseEntity<HoldingEntity> getHolding(
+        @PathVariable("holdingId") Long holdingId
+    ) {
+        HoldingEntity holding = holdingService.getHoldingByHoldingId(holdingId);
+
+        return ResponseEntity.ok(holding);
+    }
+
+    @GetMapping("/client/{clientId}")
+    public ResponseEntity<List<HoldingEntity>> getClientHoldings(
+        @PathVariable("clientId") Long clientId
+    ) {
+        List<HoldingEntity> holdings = holdingService.getHoldingsByClientId(clientId);
+
+        return ResponseEntity.ok(holdings);
+    }
+
+    @GetMapping("/instrument/{instrumentId}")
+    public ResponseEntity<List<HoldingEntity>> getInstrumentHoldings(
+        @PathVariable("instrumentId") Long instrumentId
+    ) {
+        List<HoldingEntity> holdings = holdingService.getHoldingsByInstrumentId(instrumentId);
+
+        return ResponseEntity.ok(holdings);
+    }
+    
+}
