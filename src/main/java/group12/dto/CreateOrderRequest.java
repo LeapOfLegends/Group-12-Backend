237734eb52e953d98ae19b285ef021f40e1d7aff
Administrator <chickentoe012@gmail.com>
2026-09-22 11:39:56 -0500

    initialized test suites for orders & added custom error messages to outbound json payload OB

--- src/main/java/group12/dto/CreateOrderRequest.java
+++ src/main/java/group12/dto/CreateOrderRequest.java
@@ -1,14 +1,17 @@
 package group12.dto;
 
 import group12.Entities.OrderType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record CreateOrderRequest(
 
         // will remove once authentication is implemented
-        Long clientId,
+        @NotNull(message = "Client ID is required") Long clientId,
 
-        Long instrumentId,
-        OrderType orderType,
-        Integer quantity
+        @NotNull(message = "Instrument ID is required") Long instrumentId,
+        @NotNull(message = "Order type is required") OrderType orderType,
+        @NotNull(message = "Quantity is required")
+        @Positive(message = "Quantity must be greater than zero") Integer quantity
 ) {
 }
